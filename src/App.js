a906c20c1eb8fa1d61d0849292b7070eb9427a6e
Jamie95187 <jamiewong63@googlemail.com>
2020-03-14 21:51:01 +0000

    feat: add event handle listener for name change

--- src/App.js
+++ src/App.js
@@ -1,40 +1,84 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
+// Smart, stateful components
 // functional component using hooks
-const App = props => {
-  const [ personsState, setPersonsState ] = useState({
+// const App = props => {
+//
+// const [ personsState, setPersonsState ] = useState({
+//   persons: [
+//     {name: 'Max', age: 28},
+//     {name: 'Manu', age: 27},
+//     {name: 'Steph', age: 27}
+//   ],
+//   otherState: 'some other variable'
+// });
+//
+// const switchNameHandler = (newName) => {
+//   // console.log('Was Clicked!');
+//   // Don't do this: this.state.persons[0].name = "Maximilian";
+//   setPersonsState({ persons: [
+//     {name: newName, age: 29},
+//     {name: 'Manu', age: 28},
+//     {name: 'Steph', age: 28}
+//   ]
+//   });
+// };
+
+class App extends Component {
+
+  state = {
     persons: [
-      {name: 'Max', age: 28},
-      {name: 'Manu', age: 27},
-      {name: 'Steph', age: 27}
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
     ],
-    otherState: 'some other variable'
-  });
+    otherState: 'some other value'
+  }
 
-  const switchNameHandler = () => {
-    // console.log('Was Clicked!');
-    // Don't do this: this.state.persons[0].name = "Maximilian";
-    setPersonsState({ persons: [
-      {name: 'Max', age: 29},
-      {name: 'Manu', age: 28},
-      {name: 'Steph', age: 28}
-    ]
-    });
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState( {
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    } )
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    } )
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!!!</p>
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this, "Max!")}>My Hobbies: Surfing</Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          changed={this.nameChangedHandler}/>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}/>
+      </div>
+    )
   };
-
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!!!</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}>My Hobbies: Surfing</Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}/>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-    </div>
-  )
 }
 
 export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
+// Presentational, stateless, dumb component should have more than stateful components
+
 const person = (props) => {
   return (
-    <div>
-      <p>I'm {props.name} and {props.age}!</p>
+    <div >
+      <p onClick={props.click}>I'm {props.name} and {props.age}!</p>
       <p>{props.children}</p>
+      <input type='text' onChange={props.changed} value={props.name}/>
     </div>
   )
 }
