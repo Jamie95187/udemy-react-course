7ecd5030b0d6833c5ec55896a8fea5418865627d
Jamie95187 <jamiewong63@googlemail.com>
2020-03-14 19:49:18 +0000

    feat: add button to change state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import Person from './Person/Person';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <p>This is really working!!!</p>
-      <Person name="Max" age="28">My Hobbies: Surfing</Person>
-      <Person name="Manu" age="27"/>
-      <Person name="Steph" age="27"/>
-    </div>
-  );
+class App extends Component {
+  state = {
+    persons: [
+      {name: 'Max', age: 28},
+      {name: 'Manu', age: 27},
+      {name: 'Steph', age: 27}
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was Clicked!');
+    // Don't do this: this.state.persons[0].name = "Maximilian";
+    this.setState({ persons: [
+      {name: 'Max', age: 29},
+      {name: 'Manu', age: 28},
+      {name: 'Steph', age: 28}
+    ]
+    })
+  };
+
+  render(){
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!!!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: Surfing</Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+      </div>
+    )
+  };
 }
 
 export default App;
