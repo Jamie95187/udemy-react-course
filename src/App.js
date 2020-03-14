2e29e7022d49f45b9c348e1492a7e4ad3d581496
Jamie95187 <jamiewong63@googlemail.com>
2020-03-14 18:58:47 +0000

    compon: add person component with props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
+import Person from './Person/Person';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!!!</p>
+      <Person name="Max" age="28">My Hobbies: Surfing</Person>
+      <Person name="Manu" age="27"/>
+      <Person name="Steph" age="27"/>
     </div>
   );
 }
