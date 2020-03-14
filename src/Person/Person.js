cf68ac1a3d9097925f85d94280357020845e769b
Jamie95187 <jamiewong63@googlemail.com>
2020-03-14 22:19:55 +0000

    css: inline and stylesheets

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Person.css';
 
 // Presentational, stateless, dumb component should have more than stateful components
 
 const person = (props) => {
   return (
-    <div >
+    <div className='Person'>
       <p onClick={props.click}>I'm {props.name} and {props.age}!</p>
       <p>{props.children}</p>
       <input type='text' onChange={props.changed} value={props.name}/>
