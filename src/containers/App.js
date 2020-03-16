fdadbb03cacf40bdabdea228857cdfde67e7ca5a
Jamie95187 <jamiewong63@googlemail.com>
2020-03-16 15:44:11 +0000

    splitting an app into components

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import Person from '../Components/Persons/Person/Person';
+import Persons from '../Components/Persons/Persons';
+import Cockpit from '../Components/Cockpit/Cockpit';
 import './App.css';
 
 // Smart, stateful components
@@ -26,19 +28,6 @@ import './App.css';
 //   ]
 //   });
 // };
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
-    color: black'
-  }`;
 
 class App extends Component {
 
@@ -93,34 +82,19 @@ class App extends Component {
     if (this.state.showPersons){
       persons = (
         <div>
-        {this.state.persons.map((person, index) => {
-          return <Person
-            click={() => this.deletePersonHandler(index)}
-            name={person.name}
-            age={person.age}
-            key={person.id}
-            changed={(event) => this.nameChangedHandler(event, person.id)}/>
-        })}
+          <Persons persons={this.state.persons}
+          clicked={this.deletePersonHandler}
+          changed={this.nameChangedHandler}/>
         </div>
       )
     }
 
-    // setting css to classes dynamically
-    const classes = [];
-    if(this.state.persons.length <= 2){
-      classes.push('red') // classes = ['red']
-    };
-    if(this.state.persons.length <= 1){
-      classes.push('bold') // classes = ['red', 'bold']
-    }
-
     return (
       <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(' ')}>This is really working!!!</p>
-        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonsHandler}>
-          Switch Name
-        </StyledButton>
+        <Cockpit
+          showPersons={this.state.showPersons}
+          persons={this.state.persons}
+          clicked={this.togglePersonsHandler} />
         {persons}
       </div>
     )
