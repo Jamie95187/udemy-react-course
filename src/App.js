5dbc11abb576d3ba4521ebaa9d3854b55cb0d22b
Jamie95187 <jamiewong63@googlemail.com>
2020-03-16 01:11:20 +0000

    add radium library to style button

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Radium from 'radium';
 import Person from './Person/Person';
 import './App.css';
 
@@ -80,7 +81,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     };
 
     let persons = null;
@@ -100,6 +105,10 @@ class App extends Component {
       )
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
     }
 
     // setting css to classes dynamically
@@ -124,4 +133,4 @@ class App extends Component {
   };
 }
 
-export default App;
+export default Radium(App);
